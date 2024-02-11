072705fd557860555b3a077154fab775166875e2
Pol Ken <pol.ken.galceran@estudiantat.upc.edu>
2024-02-11 21:13:49 +0100

    types changed to books

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ func main() {
 	}
 	defer db.Close()
 
-	_, err = db.Exec("CREATE TABLE IF NOT EXISTS books (id SERIAL PRIMARY KEY, title TEXT, author TEXT)")
+	_, err = db.Exec("CREATE TABLE IF NOT EXISTS books (id SERIAL PRIMARY KEY, titol TEXT, autor TEXT, prestatge TEXT, posicio TEXT, habitacio TEXT, tipus TEXT, editorial TEXT, idioma TEXT, notes TEXT)")
 
 	fmt.Println("Server is running on port 8000")
 
@@ -53,7 +53,7 @@ func getBooks(db *sql.DB) http.HandlerFunc {
 		books := make([]Book, 0)
 		for rows.Next() {
 			book := Book{}
-			err := rows.Scan(&book.ID, &book.Title, &book.Author)
+			err := rows.Scan(&book.ID, &book.Titol, &book.Autor, &book.Prestatge, &book.Posicio, &book.Habitacio, &book.Tipus, &book.Editorial, &book.Idioma, &book.Notes)
 			if err != nil {
 				http.Error(w, http.StatusText(500), 500)
 				return
@@ -80,8 +80,7 @@ func createBook(db *sql.DB) http.HandlerFunc {
 			return
 		}
 
-
-		_, err = db.Exec("INSERT INTO books (title, author) VALUES ($1, $2)", book.Title, book.Author)
+		_, err = db.Exec("INSERT INTO books (titol, autor, prestatge, posicio, habitacio, tipus, editorial, idioma, notes) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)", book.Titol, book.Autor, book.Prestatge, book.Posicio, book.Habitacio, book.Tipus, book.Editorial, book.Idioma, book.Notes)
 		if err != nil {
 			http.Error(w, http.StatusText(500), 500)
 			return
@@ -100,7 +99,7 @@ func updateBook(db *sql.DB) http.HandlerFunc {
 			return
 		}
 
-		_, err = db.Exec("UPDATE books SET title = $1, author = $2 WHERE id = $3", book.Title, book.Author, book.ID)
+		_, err = db.Exec("UPDATE books SET titol = $1, autor = $2, prestatge = $3, posicio = $4, habitacio = $5, tipus = $6, editorial = $7, idioma = $8, notes = $9 WHERE id = $10", book.Titol, book.Autor, book.Prestatge, book.Posicio, book.Habitacio, book.Tipus, book.Editorial, book.Idioma, book.Notes, book.ID)
 		if err != nil {
 			http.Error(w, http.StatusText(500), 500)
 			return
--- types.go
+++ types.go
@@ -1,7 +1,14 @@
 package main
 
 type Book struct {
-	ID     int    `json:"id"`
-	Title  string `json:"title"`
-	Author string `json:"author"`
+	ID        int    `json:"id"`
+	Titol     string `json:"titol"`
+	Autor     string `json:"autor"`
+	Prestatge string `json:"prestatge"`
+	Posicio   string `json:"posicio"`
+	Habitacio string `json:"habitacio"`
+	Tipus     string `json:"tipus"`
+	Editorial string `json:"editorial"`
+	Idioma    string `json:"idioma"`
+	Notes     string `json:"notes"`
 }

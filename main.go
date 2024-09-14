735db42b8e8311c67e5540c8cf774d7137f810d7
polkeng2 <polkengalce2@gmail.com>
2024-09-14 19:43:23 +0200

    auth fix

--- main.go
+++ main.go
@@ -51,7 +51,7 @@ func enableCORS(next http.Handler) http.Handler {
 		// Set CORS headers
 		w.Header().Set("Access-Control-Allow-Origin", "*") // Allow any origin
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
 		// Check if the request is for CORS preflight
 		if r.Method == "OPTIONS" {
